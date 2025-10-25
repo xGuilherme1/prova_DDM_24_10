6dea27cf85585b42e6360fb8aca11d434d6a9380
xGuilherme1 <guilherme2019assuncao@gmail.com>
2025-10-24 20:42:04 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,17 +1,18 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import LoginScreen from "./screens/LoginScreen.tsx";
-import RegisterScreen from "./screens/RegisterScreen.tsx";
-import HomeScreen from "./screens/HomeScreen.tsx";
+
+import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
+import HomeScreen from "./screens/HomeScreen";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
       <NavigationContainer>
-        <Stack.Navigator id={1}>
-          <Stack.Screen name="Login" component={lloginScreen} />
+        <Stack.Navigator>
+          <Stack.Screen name="Login" component={LoginScreen} />
           <Stack.Screen name="Register" component={RegisterScreen} />
           <Stack.Screen name="Home" component={HomeScreen} />
         </Stack.Navigator>
